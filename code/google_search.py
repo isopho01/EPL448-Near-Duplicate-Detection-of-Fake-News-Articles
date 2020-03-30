d7f65dda31df96589a82ad1be82db5d7e571a5b0
isopho01 <isopho01@cs.ucy.ac.cy>
2020-03-30 11:20:30 +0300

    Google Search

--- code/google_search.py
+++ code/google_search.py
@@ -1,50 +1,21 @@
-import os
-import sys
-
 import requests
 from bs4 import BeautifulSoup
-import json
-import time
-import random
-from csv import writer
-from csv import DictWriter
+import pprint
 
 
 class GoogleSearch:
-    def __init__(self, filename="data.csv"):
+    def __init__(self):
         self.USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
         self.MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, " \
                                  "like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36 "
-        self.numOfQueries = 0
-        self.timeout = 0
-        self.filename = filename
-
-    def append_list_as_row(self, list_of_elem):
-
-        # Write header
-        if not os.path.exists(self.filename):
-            with open(self.filename, 'w') as f:
-                f.write('query,results\n')
-
-        # Open file in append mode
-        with open(self.filename, 'a+', newline='') as write_obj:
-            # Create a writer object from csv module
-            csv_writer = writer(write_obj)
-            # Add dictionary as wor in the csv
-            csv_writer.writerow(list_of_elem)
 
     def run(self, query):
-        encoded_query = query.replace(' ', '+')
-        url = f"https://google.com/search?q={encoded_query}&lr=lang_en&num=100&start=0"
+        query = query.replace(' ', '+')
+        url = f"https://google.com/search?q={query}&lr=lang_en&num=100&start=0"
 
         headers = {"user-agent": self.USER_AGENT}
         resp = requests.get(url, headers=headers)
         results = []
-
-        if self.timeout < 5:
-            time.sleep(random.randrange(2, 7))  # Random delay in seconds
-        else:
-            sys.exit(f"Timeout at query {self.numOfQueries}")
 
         if resp.status_code == 200:
             soup = BeautifulSoup(resp.content, "html.parser")
@@ -53,15 +24,11 @@ class GoogleSearch:
                 if anchors:
                     results.append(anchors[0]['href'])
 
-            # Add to csv
-            self.append_list_as_row([query, results])
-
-            print(self.numOfQueries)
-            self.numOfQueries = self.numOfQueries + 1
-            self.timeout = 0
+            return results
         else:
             print(f"HTTP Error {resp.status_code}")
-            self.timeout = self.timeout + 1
+
+        return None
 
 
 def main():
@@ -77,11 +44,7 @@ def main():
                "Target to Discontinue Sale of Holy Bible"]
 
     google = GoogleSearch()
-    [google.run(j) for j in queries]
-    # dictionary = [{j: google.run(j)} for j in queries]
-
-    # with open('data2.json', 'w') as outfile:
-    #     json.dump(dictionary, outfile, sort_keys=True, indent=4)
+    [pprint.pprint(google.run(j)) for j in queries]
 
 
 if __name__ == '__main__':
