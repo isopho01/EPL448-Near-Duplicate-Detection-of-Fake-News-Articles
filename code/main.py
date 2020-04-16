459ec163f1f0a12614264ad4e29a33f77a02e6cc
isopho01 <isopho01@cs.ucy.ac.cy>
2020-04-16 17:09:21 +0300

    comments

--- code/main.py
+++ code/main.py
@@ -59,10 +59,11 @@ def preprocessing(sentence):
 
 
 def main():
-    # data = readJsonData('./dataset/politifact_results.json')
-    # if data.empty:
-    #     sys.exit('File not found')
-    # print(data)
+    data = readJsonData('./dataset/politifact_results.json')
+    if data.empty:
+        sys.exit('File not found')
+    print(data)
+    
     sentence = "Western countries are being urged to intervene in a case where 6 young school girls facing execution for acting indecently at a friends house.\n\nFathima Al Kwaini and her friends that included three male friends have celebrated\n\nKwaini's\n\nbirthday at a friends house. A neighbor supposedly an assistant of an Imam of a mosque close by has reported this to Saudi Arabia's religious police. When the police arrived the girls were dancing with their male friends and they were arrested immediately.\n\n\n\n\n\n\n\n\n\nThe ultra conservative Arabian nation that has one of the worst human rights records is also a member of the United Nations Human Rights commission and recently got elected to the Women's Rights Commission as well which sparked anger and protest.\n\nAccording to HRW the girls were detained for more than a year before the trial and never confessed committing any crime. However the verdict of the \"male only\" Sharia panel was that they need o be executed in accordance with the Sharia law. The boys were only advised \"not to be victimized\" the report further states.\n\n\n\n\n\nSaudi uses methods such as beheading, stoning and crucifixion to execute women for crimes, including adultery, in the strict Islamic country. Beheadings take place in public squares where the headless corpses are later put on display."
     print(preprocessing(sentence))
 
